d3192fb0f030ed156cf5caf843be35604b0c1de7
Mario Gasca Lopez <web@mariogasca.com>
2021-04-14 15:59:16 -0500

    Modificacion de la primer aplicacion

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
-export class Tab3Page {
+export class Tab3Page implements OnInit{
 
   constructor() {}
 
+  ngOnInit(){
+
+    let numero = 10;
+
+    numero = 20;
+    
+    numero += 10;
+
+    numero = 0;
+
+    console.log(numero);
+  }
 }
